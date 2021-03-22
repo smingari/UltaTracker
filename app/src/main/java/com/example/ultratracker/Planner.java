88ca9b1a90af757fdb4216561ef640f8882a7a0c
smingari <mingari@wisc.edu>
2021-03-21 19:12:21 -0500

    Used Task from Iteration and got planner from Feature_Planner

---
+++ app/src/main/java/com/example/ultratracker/Planner.java
@@ -0,0 +1,38 @@
+package com.example.ultratracker;
+
+import java.util.ArrayList;
+
+public class Planner {
+
+    private ArrayList<Task> todo;
+
+    public Planner () {
+        todo = new ArrayList<Task>();
+    }
+
+
+    public void addTask(Task newTask) {
+        todo.add(newTask);
+    }
+
+    public void removeTask(Task task) {
+        if(todo.contains(task)) {
+            todo.remove(task);
+        }
+    }
+
+    public void editTask(Task task, String newText) {
+        if(todo.contains(task)) {
+            int index = todo.indexOf(task);
+            todo.get(index).setTaskText(newText);
+        }
+    }
+
+    public void markAsComplete(Task task) {
+        if(todo.contains(task)) {
+            int index = todo.indexOf(task);
+            todo.get(index).setCompleted(true);
+        }
+    }
+
+}
