967ee9466279cdbb19e0e2330c6fdbb132b9fe5b
fedosky <fedosky@wisc.edu>
2021-03-24 19:22:48 -0500

    Fixed table layout issues

--- app/src/main/java/com/example/ultratracker/PDayActivity.java
+++ app/src/main/java/com/example/ultratracker/PDayActivity.java
@@ -25,15 +25,16 @@ public class PDayActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_p_day);
 
-        // Create the table of tasks programmatically
-        init_table();
-
         setContentView(R.layout.activity_p_day);
         TextView date = (TextView)findViewById(R.id.current_date);
         date.setText(MainActivity.selectedDate);
+
+        // Create the table of tasks programmatically
+        init_task_table();
+        init_completed_table();
     }
 
-    public void init_table() {
+    public void init_task_table() {
         TableLayout taskTable = findViewById(R.id.task_table);
         TaskDatabaseHelper taskDatabaseHelper = new TaskDatabaseHelper(this);
         List<Task> taskList = taskDatabaseHelper.getAll();
@@ -47,16 +48,19 @@ public class PDayActivity extends AppCompatActivity {
         // First column header
         TextView tv0 = new TextView(this);
         tv0.setText(" Task ");
+        tv0.setGravity(Gravity.CENTER_HORIZONTAL);
         taskTableHeader.addView(tv0);
 
         // Second column header
         TextView tv1 = new TextView(this);
         tv1.setText(" Due Date ");
+        tv1.setGravity(Gravity.CENTER_HORIZONTAL);
         taskTableHeader.addView(tv1);
 
         // Third column header
         TextView tv2 = new TextView(this);
         tv2.setText(" Priority Level ");
+        tv2.setGravity(Gravity.CENTER_HORIZONTAL);
         taskTableHeader.addView(tv2);
 
         // Add header row to table
@@ -68,17 +72,67 @@ public class PDayActivity extends AppCompatActivity {
 
             TextView t1v = new TextView(this);
             t1v.setText(taskList.get(i).getName());
-            t1v.setGravity(Gravity.CENTER);
             row.addView(t1v);
 
             TextView t2v = new TextView(this);
             t1v.setText(taskList.get(i).getDueDate());
-            t1v.setGravity(Gravity.CENTER);
             row.addView(t2v);
 
             TextView t3v = new TextView(this);
             t3v.setText(taskList.get(i).getPriority());
-            t3v.setGravity(Gravity.CENTER);
+            row.addView(t3v);
+
+            taskTable.addView(row);
+        }
+
+    }
+
+    public void init_completed_table() {
+        TableLayout taskTable = findViewById(R.id.completed_table);
+        TaskDatabaseHelper taskDatabaseHelper = new TaskDatabaseHelper(this);
+        List<Task> taskList = taskDatabaseHelper.getAll();
+        int dbSize = taskDatabaseHelper.getAll().size();
+
+        // Set up table header
+        TableRow taskTableHeader = new TableRow(this);
+        TableRow.LayoutParams lp = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT);
+        taskTableHeader.setLayoutParams(lp);
+
+        // First column header
+        TextView tv0 = new TextView(this);
+        tv0.setText(" Task ");
+        tv0.setGravity(Gravity.CENTER_HORIZONTAL);
+        taskTableHeader.addView(tv0);
+
+        // Second column header
+        TextView tv1 = new TextView(this);
+        tv1.setText(" Due Date ");
+        tv1.setGravity(Gravity.CENTER_HORIZONTAL);
+        taskTableHeader.addView(tv1);
+
+        // Third column header
+        TextView tv2 = new TextView(this);
+        tv2.setText(" Priority Level ");
+        tv2.setGravity(Gravity.CENTER_HORIZONTAL);
+        taskTableHeader.addView(tv2);
+
+        // Add header row to table
+        taskTable.addView(taskTableHeader);
+
+        // Add rows dynamically from database
+        for (int i = 0; i < dbSize; i++) {
+            TableRow row = new TableRow(this);
+
+            TextView t1v = new TextView(this);
+            t1v.setText(taskList.get(i).getName());
+            row.addView(t1v);
+
+            TextView t2v = new TextView(this);
+            t1v.setText(taskList.get(i).getDueDate());
+            row.addView(t2v);
+
+            TextView t3v = new TextView(this);
+            t3v.setText(taskList.get(i).getPriority());
             row.addView(t3v);
 
             taskTable.addView(row);
