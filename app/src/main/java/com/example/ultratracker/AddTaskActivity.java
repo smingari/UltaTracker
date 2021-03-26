606c1e5d2e7a1ec7ee2b454fce3fc32052d94a0b
jonatru <jonathon.truttmann@gmail.com>
2021-03-25 22:41:28 -0500

    Changed priority editor to a spinner

--- app/src/main/java/com/example/ultratracker/AddTaskActivity.java
+++ app/src/main/java/com/example/ultratracker/AddTaskActivity.java
@@ -7,18 +7,26 @@ import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AddTaskActivity extends AppCompatActivity implements DateSelectorDialog.DateSelectorListener {
     Button cancel_button, create_task_button, edit_date_button;
-    EditText name_entry, due_time_entry, description_entry, priority_entry;
+    EditText name_entry, due_time_entry, description_entry;
     TextView date_display;
+    Spinner priority_entry;
+
+    int priority;
 
     private int taskSelectedYear;
     private int taskSelectedMonth;
@@ -53,8 +61,6 @@ public class AddTaskActivity extends AppCompatActivity implements DateSelectorDi
                     String name = name_entry.getText().toString();
                     String due_time = due_time_entry.getText().toString();
                     String description = description_entry.getText().toString();
-                    String prior = priority_entry.getText().toString();
-                    int priority = Integer.parseInt(prior);
 
                     LocalDate syn_date = LocalDate.of(taskSelectedYear,taskSelectedMonth,taskSelectedDay);
                     LocalTime syn_time = LocalTime.parse(due_time.length() < 5 ? "0" + due_time : due_time); //quick and dirty hack plz remove
@@ -74,6 +80,30 @@ public class AddTaskActivity extends AppCompatActivity implements DateSelectorDi
             @Override
             public void onClick(View v) {
                 openDialog();
+            }
+        });
+
+        final List<Integer> priorities = new ArrayList<Integer>();
+
+        priorities.add(1);
+        priorities.add(2);
+        priorities.add(3);
+        priorities.add(4);
+        priorities.add(5);
+
+        ArrayAdapter<Integer> dataAdapter = new ArrayAdapter<Integer>(this, android.R.layout.simple_spinner_item, priorities);
+        priority_entry.setAdapter(dataAdapter);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        priority_entry.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                priority = priorities.get(position);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
             }
         });
     }
