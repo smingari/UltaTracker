04440e462009b12c825a4730b699153aea72fe4d
jonatru <jonathon.truttmann@gmail.com>
2021-04-09 17:59:13 -0500

    Added remove and get all methods to ExerciseDatabaseHelper

--- app/src/main/java/com/example/ultratracker/ExerciseDatabaseHelper.java
+++ app/src/main/java/com/example/ultratracker/ExerciseDatabaseHelper.java
@@ -18,6 +18,7 @@ public class ExerciseDatabaseHelper extends SQLiteOpenHelper {
     public static final String COLUMN_EXERCISE_TYPE = "EXERCISE_TYPE";
     public static final String COLUMN_EXERCISE_DATE = "EXERCISE_DATE";
     public static final String COLUMN_EXERCISE_TIME = "EXERCISE_TIME";
+    public static final String COLUMN_EXERCISE_DURATION = "EXERCISE_DURATION";
     public static final String COLUMN_EXERCISE_CALS = "EXERCISE_CALS";
 
     public static final String WEIGHTLIFTING_TABLE = "WEIGHTLIFTING_TABLE";
@@ -50,7 +51,7 @@ public class ExerciseDatabaseHelper extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
         String createExerciseTableStatement = "CREATE TABLE " + EXERCISE_TABLE + " (" + COLUMN_EXERCISE_ID + " INTEGER UNIQUE PRIMARY KEY AUTOINCREMENT, " +
                 COLUMN_EXERCISE_KEY + " INT, " + COLUMN_EXERCISE_TYPE + " TEXT, " + COLUMN_EXERCISE_DATE + " Text, " + COLUMN_EXERCISE_TIME + " Text, " +
-                COLUMN_EXERCISE_CALS + " INT) ";
+                COLUMN_EXERCISE_TIME + " INT, " + COLUMN_EXERCISE_CALS + " INT) ";
         String createWeightliftingTableStatement = "CREATE TABLE " + WEIGHTLIFTING_TABLE + " (" + COLUMN_WEIGHTLIFTING_ID + " INTEGER UNIQUE PRIMARY KEY AUTOINCREMENT, " +
                 COLUMN_WEIGHTLIFTING_KEY + " INT, " + COLUMN_WEIGHTLIFTING_NAME + " TEXT, " + COLUMN_WEIGHTLIFTING_SETS + " INT, " + COLUMN_WEIGHTLIFTING_REPS + " INT, " +
                 COLUMN_WEIGHTLIFTING_WEIGHT + " INT " + ")";
@@ -81,7 +82,8 @@ public class ExerciseDatabaseHelper extends SQLiteOpenHelper {
         int key;
         String exerciseType;
         String completedDate;
-        String duration;
+        String completedTime;
+        int duration;
         int caloriesBurned;
 
         // move to the first result. If it is true then there is at least 1 value
@@ -90,24 +92,25 @@ public class ExerciseDatabaseHelper extends SQLiteOpenHelper {
                 key = cursor.getInt(1);
                 exerciseType = cursor.getString(2);
                 completedDate = cursor.getString(3);
-                duration = cursor.getString(4);
-                caloriesBurned = cursor.getInt(5);
+                completedTime = cursor.getString(4);
+                duration = cursor.getInt(5);
+                caloriesBurned = cursor.getInt(6);
 
                 switch (exerciseType) {
                     case "Ride":
-                        Ride ride = getRide(key, completedDate, duration, caloriesBurned);
+                        Ride ride = getRide(key, completedDate, completedTime, duration, caloriesBurned);
                         returnList.add(ride);
                         break;
                     case "Run":
-                        Run run = getRun(key, completedDate, duration, caloriesBurned);
+                        Run run = getRun(key, completedDate, completedTime, duration, caloriesBurned);
                         returnList.add(run);
                         break;
                     case "Weightlifting":
-                        WeightliftingWorkout ww = getWeightliftingWorkout(key, completedDate, duration, caloriesBurned);
+                        WeightliftingWorkout ww = getWeightliftingWorkout(key, completedTime, completedDate, duration, caloriesBurned);
                         returnList.add(ww);
                         break;
                     default:
-                        Exercise e = new Exercise(exerciseType, completedDate, duration, caloriesBurned);
+                        Exercise e = new Exercise(exerciseType, completedDate, completedTime, duration, caloriesBurned);
                         e.setKey(key);
                         break;
                 }
@@ -119,7 +122,7 @@ public class ExerciseDatabaseHelper extends SQLiteOpenHelper {
         return returnList;
     }
 
-    public Ride getRide(int key, String completedDate, String duration, int caloriesBurned) {
+    public Ride getRide(int key, String completedDate, String completedTime, int duration, int caloriesBurned) {
         Ride ride = null;
         String queryString = "SELECT * FROM " + RIDE_TABLE + " WHERE " + COLUMN_RIDE_KEY + " = ?";
         SQLiteDatabase db = this.getReadableDatabase();
@@ -132,7 +135,7 @@ public class ExerciseDatabaseHelper extends SQLiteOpenHelper {
         if (cursor.moveToFirst()) {
             distance = cursor.getDouble(2);
             pace = cursor.getDouble(3);
-            ride = new Ride(completedDate, duration, caloriesBurned, distance, pace);
+            ride = new Ride(completedDate, completedTime, duration, caloriesBurned, distance, pace);
             ride.setKey(key);
         }
 
@@ -141,7 +144,7 @@ public class ExerciseDatabaseHelper extends SQLiteOpenHelper {
         return ride;
     }
 
-    public Run getRun(int key, String completedDate, String duration, int caloriesBurned) {
+    public Run getRun(int key, String completedDate, String completedTime, int duration, int caloriesBurned) {
         Run run = null;
         String queryString = "SELECT * FROM " + RUN_TABLE + " WHERE " + COLUMN_RUN_KEY + " = ?";
         SQLiteDatabase db = this.getReadableDatabase();
@@ -154,7 +157,7 @@ public class ExerciseDatabaseHelper extends SQLiteOpenHelper {
         if (cursor.moveToFirst()) {
             distance = cursor.getDouble(2);
             pace = cursor.getDouble(3);
-            run = new Run(completedDate, duration, caloriesBurned, distance, pace);
+            run = new Run(completedDate, completedTime, duration, caloriesBurned, distance, pace);
             run.setKey(key);
         }
 
@@ -163,10 +166,10 @@ public class ExerciseDatabaseHelper extends SQLiteOpenHelper {
         return run;
     }
 
-    public WeightliftingWorkout getWeightliftingWorkout(int key, String completedDate, String duration, int caloriesBurned) {
+    public WeightliftingWorkout getWeightliftingWorkout(int key, String completedDate, String completedTime, int duration, int caloriesBurned) {
         WeightliftingWorkout ww;
         List<Weightlifting> wlList = getWeightlifting(key);
-        ww = new WeightliftingWorkout(completedDate, duration, caloriesBurned, wlList);
+        ww = new WeightliftingWorkout(completedDate, completedTime, duration, caloriesBurned, wlList);
         ww.setKey(key);
         return ww;
     }
@@ -208,11 +211,13 @@ public class ExerciseDatabaseHelper extends SQLiteOpenHelper {
         cv.put(COLUMN_EXERCISE_TYPE, "Weightlifting");
         cv.put(COLUMN_EXERCISE_DATE, ww.getCompletedDate());
         cv.put(COLUMN_EXERCISE_TIME, ww.getCompletedTime());
+        cv.put(COLUMN_EXERCISE_DURATION, ww.getDuration());
         cv.put(COLUMN_EXERCISE_CALS, ww.getCaloriesBurned());
         for(Weightlifting w : ww.getExerciseList()) {
             addWeightlifting(w, ww.getKey());
         }
         long insert = db.insert(EXERCISE_TABLE, null, cv);
+        db.close();
         return insert != -1;
     }
 
@@ -227,6 +232,7 @@ public class ExerciseDatabaseHelper extends SQLiteOpenHelper {
         cv.put(COLUMN_WEIGHTLIFTING_WEIGHT, w.getWeight());
 
         long insert = db.insert(WEIGHTLIFTING_TABLE, null, cv);
+        db.close();
         return insert != -1;
     }
 
@@ -245,9 +251,12 @@ public class ExerciseDatabaseHelper extends SQLiteOpenHelper {
         cv1.put(COLUMN_EXERCISE_TYPE, "Ride");
         cv1.put(COLUMN_EXERCISE_DATE, ride.getCompletedDate());
         cv1.put(COLUMN_EXERCISE_TIME, ride.getCompletedTime());
+        cv1.put(COLUMN_EXERCISE_DURATION, ride.getDuration());
         cv1.put(COLUMN_EXERCISE_CALS, ride.getCaloriesBurned());
 
-        return db.insert(EXERCISE_TABLE, null, cv) != -1;
+        long insert = db.insert(EXERCISE_TABLE, null, cv);
+        db.close();
+        return insert != -1;
     }
 
     public boolean addRun(Run run) {
@@ -265,21 +274,36 @@ public class ExerciseDatabaseHelper extends SQLiteOpenHelper {
         cv1.put(COLUMN_EXERCISE_TYPE, "Run");
         cv1.put(COLUMN_EXERCISE_DATE, run.getCompletedDate());
         cv1.put(COLUMN_EXERCISE_TIME, run.getCompletedTime());
+        cv1.put(COLUMN_EXERCISE_DURATION, run.getDuration());
         cv1.put(COLUMN_EXERCISE_CALS, run.getCaloriesBurned());
 
-        return db.insert(EXERCISE_TABLE, null, cv) != -1;
+        long insert = db.insert(EXERCISE_TABLE, null, cv);
+        db.close();
+        return insert != -1;
     }
 
     public boolean removeWeightliftingWorkout(WeightliftingWorkout ww) {
-        return false;
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete(WEIGHTLIFTING_TABLE, COLUMN_WEIGHTLIFTING_KEY + "=", new String[]{String.valueOf(ww.getKey())});
+        final int delete = db.delete(EXERCISE_TABLE, COLUMN_EXERCISE_KEY + "=", new String[] {String.valueOf(ww.getKey())});
+        db.close();
+        return delete > 0;
     }
 
     public boolean removeRide(Ride ride) {
-        return false;
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete(RIDE_TABLE, COLUMN_RIDE_KEY + "=", new String[]{String.valueOf(ride.getKey())});
+        final int delete = db.delete(EXERCISE_TABLE, COLUMN_EXERCISE_KEY + "=", new String[] {String.valueOf(ride.getKey())});
+        db.close();
+        return delete > 0;
     }
 
     public boolean removeRun(Run run) {
-        return false;
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete(RUN_TABLE, COLUMN_RUN_KEY + "=", new String[]{String.valueOf(run.getKey())});
+        final int delete = db.delete(EXERCISE_TABLE, COLUMN_EXERCISE_KEY + "=", new String[] {String.valueOf(run.getKey())});
+        db.close();
+        return delete > 0;
     }
 
     public boolean editWeightliftingWorkout(WeightliftingWorkout ww) {
@@ -295,6 +319,54 @@ public class ExerciseDatabaseHelper extends SQLiteOpenHelper {
     }
 
     public List<Exercise> getAll() {
-        return null;
+        List<Exercise> returnList = new ArrayList<>();
+
+        // get data from the database
+        String queryString = "SELECT * FROM " + EXERCISE_TABLE;
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = db.rawQuery(queryString, null);
+
+        int key;
+        String exerciseType;
+        String completedDate;
+        String completedTime;
+        int duration;
+        int caloriesBurned;
+
+        // move to the first result. If it is true then there is at least 1 value
+        if (cursor.moveToFirst()) {
+            do {
+                key = cursor.getInt(1);
+                exerciseType = cursor.getString(2);
+                completedDate = cursor.getString(3);
+                completedTime = cursor.getString(4);
+                duration = cursor.getInt(5);
+                caloriesBurned = cursor.getInt(6);
+
+                switch (exerciseType) {
+                    case "Ride":
+                        Ride ride = getRide(key, completedDate, completedTime, duration, caloriesBurned);
+                        returnList.add(ride);
+                        break;
+                    case "Run":
+                        Run run = getRun(key, completedDate, completedTime, duration, caloriesBurned);
+                        returnList.add(run);
+                        break;
+                    case "Weightlifting":
+                        WeightliftingWorkout ww = getWeightliftingWorkout(key, completedTime, completedDate, duration, caloriesBurned);
+                        returnList.add(ww);
+                        break;
+                    default:
+                        Exercise e = new Exercise(exerciseType, completedDate, completedTime, duration, caloriesBurned);
+                        e.setKey(key);
+                        break;
+                }
+            } while(cursor.moveToNext());
+        }
+
+        cursor.close();
+        db.close();
+        return returnList;
     }
 }
